3c9c7e5b3b79c059c380e1b540d90ffb6ed51077
Diya Gupta <141258980+Diya-Gupta26@users.noreply.github.com>
2025-06-25 11:16:54 +0530

    initial edit

--- app/src/main/java/com/diya/project/realtimeweather/MainActivity.kt
+++ app/src/main/java/com/diya/project/realtimeweather/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             RealtimeWeatherTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
